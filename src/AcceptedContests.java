565bb31bbef1bd46f6700f5929c2d848e29053c6
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-18 01:36:32 +0300

    B - Basic Data Types accepted contest

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -1,7 +1,20 @@
 public class AcceptedContests {
-    /*
+    /* A - Say Hello With C++
     * Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         System.out.println("Hello, "+s);
         */
+/*B - Basic Data Types
+Scanner scanner = new Scanner(System.in);
+        int intvariable = scanner.nextInt();
+        long longvariable = scanner.nextLong();
+        char charvariable = scanner.next().charAt(0);
+        float floatvariable = scanner.nextFloat();
+        double doubvariable = scanner.nextDouble()  ;
+        System.out.println(intvariable);
+        System.out.println(longvariable);
+        System.out.println(charvariable);
+        System.out.println(floatvariable);
+        System.out.println(doubvariable);
+* */
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x + " + " + y +" = "+ (x+y));
+        System.out.println(x + " * " + y +" = "+ (x*y));
+        System.out.println(x + " - " + y +" = "+ (x-y));
 
     }
 }
