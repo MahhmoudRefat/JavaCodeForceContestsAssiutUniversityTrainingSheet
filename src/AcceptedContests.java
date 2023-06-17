21abab6c1898f0abd5a6aee66a7471c2d25e3f45
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-18 01:22:20 +0300

    new class for the accepted contest

---
+++ src/AcceptedContests.java
@@ -0,0 +1,7 @@
+public class AcceptedContests {
+    /*
+    * Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("Hello, "+s);
+        */
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        System.out.println("Hello, "+s);
+
     }
 }
