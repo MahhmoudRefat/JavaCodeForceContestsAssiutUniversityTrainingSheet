3e7dc87552bfb11a84898e9c7cb676034032d06a
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-19 00:45:45 +0300

    calculate contest

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        int y = scanner.nextInt();
+        long y = scanner.nextLong();
         System.out.println(x + " + " + y +" = "+ (x+y));
         System.out.println(x + " * " + y +" = "+ (x*y));
         System.out.println(x + " - " + y +" = "+ (x-y));
