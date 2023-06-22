6fba3a71286c6023c61f1f496cd564f3c0bdbe94
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-22 19:36:30 +0300

    summation from 1 to N 	 problem code force

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long x = scanner.nextInt();
+        System.out.println((x*(x+1))/2);
 
     }
 }
