f73b23e0b403f06e72645bbb201b25e2aa6c3336
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-02 11:19:00 +0300

    M. Capital or Small or Digit Problem code force

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -96,4 +96,22 @@ Scanner scanner = new Scanner(System.in);
             System.out.println("NOT");
         }
      */
+    /*
+        M. Capital or Small or Digit
+
+       Scanner scanner = new Scanner(System.in);
+         char x = scanner.next().charAt(0);
+        if (x >= 48 && x <=64)
+        {
+            System.out.println("IS DIGIT");
+        }
+    else if (x >= 65 && x <= 96){
+            System.out.println("ALPHA");
+            System.out.println("IS CAPITAL");
+        }
+    else {
+            System.out.println("ALPHA");
+            System.out.println("IS SMALL");
+        }
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         char x = scanner.next().charAt(0);
-        if (x >= 48 && x <=64)
-        {
-            System.out.println("IS DIGIT");
-        }
-    else if (x >= 65 && x <= 96){
-            System.out.println("ALPHA");
-            System.out.println("IS CAPITAL");
-        }
-    else {
-            System.out.println("ALPHA");
-            System.out.println("IS SMALL");
-        }
+
     }
 }
