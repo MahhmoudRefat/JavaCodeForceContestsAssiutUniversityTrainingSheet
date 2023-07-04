576ca52ba1e3054f2ba78fb5db42e10c022aef96
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-04 16:15:50 +0300

    N - Char  Problem code force

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        char x = scanner.next().charAt(0);
+        int ascii = (int) x;
+        int x2 = 0;
+        if (ascii >= 65 && ascii <= 96) {
+            x2 = x + 32;
+        } else {
+            x2 = x - 32;
+        }
+        char ch = (char) x2;
+        System.out.println(ch);
     }
 }
