5426c011e54eb02886a45f16d5fb6a7c80265315
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-06 23:23:13 +0300

    Q. Coordinates of a Point in class  Problem code force

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,6 @@ public class Main {
         float x = scanner.nextFloat();
         float y = scanner.nextFloat();
 
-        if (x == 0.0 && y == 0.0) {
-            System.out.println("Origem");
-        } else if (x != 0f && y == 0f) {
-            System.out.println("Eixo X");
-        } else if (x == 0f && y != 0f) {
-            System.out.println("Eixo Y");
-        } else if (x > 0 && y > 0) {
-            System.out.println("Q1");
-        } else if (x < 0 && y > 0) {
-            System.out.println("Q2");
-        } else if (x > 0 && y < 0) {
-            System.out.println("Q4");
-        } else if (x < 0 && y < 0) {
-            System.out.println("Q3");
-        }
     }
 }
 /*
