102f710e5de9f42f18752fb2ff075f62225aab24
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-06 23:22:13 +0300

    Q. Coordinates of a Point  Problem code force

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -114,4 +114,18 @@ Scanner scanner = new Scanner(System.in);
             System.out.println("IS SMALL");
         }
      */
+
+    /*
+    Scanner scanner = new Scanner(System.in);
+        char x = scanner.next().charAt(0);
+        int ascii = (int) x;
+        int x2 = 0;
+        if (ascii >= 65 && ascii <= 96) {
+            x2 = x + 32;
+        } else {
+            x2 = x - 32;
+        }
+        char ch = (char) x2;
+        System.out.println(ch);
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char x = scanner.next().charAt(0);
-        int ascii = (int) x;
-        int x2 = 0;
-        if (ascii >= 65 && ascii <= 96) {
-            x2 = x + 32;
-        } else {
-            x2 = x - 32;
+        float x = scanner.nextFloat();
+        float y = scanner.nextFloat();
+
+        if (x == 0.0 && y == 0.0) {
+            System.out.println("Origem");
+        } else if (x != 0f && y == 0f) {
+            System.out.println("Eixo X");
+        } else if (x == 0f && y != 0f) {
+            System.out.println("Eixo Y");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
         }
-        char ch = (char) x2;
-        System.out.println(ch);
     }
 }
+/*
+  String input = scanner.nextLine();
+        char a = input.charAt(0);
+        char s = input.charAt(1);
+        char b = input.charAt(2);
+
+    int a = scanner.nextInt();
+    char s = scanner.next().charAt(0);
+    int b = scanner.nextInt();
+    int result = 0;
+        if (s == '+') {
+                result = a + b;
+                } else if (s == '−') {
+                result = a - b;
+                } else if (s == '*') {
+                result = a * b;
+                } else {
+                result = a / b;
+                }
+                System.out.println(result);
+
+
+ */
