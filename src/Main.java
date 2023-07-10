79d9248c7da2832963c21cfc432af69f4c79b4b4
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-11 00:54:29 +0300

    U. Float or int Problem code force

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float x = scanner.nextFloat();
-        float y = scanner.nextFloat();
-
+        int parsx = (int) x;
+        float f = x - parsx;
+        if (x - parsx > 0) {
+            System.out.print("float " + parsx );
+            System.out.printf(" %.3f", f);
+        } else {
+            System.out.println("int " + parsx);
+        }
     }
 }
 /*
