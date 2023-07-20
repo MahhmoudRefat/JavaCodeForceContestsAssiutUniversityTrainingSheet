e96b8907c98751cbee7240dca96e8716e57fa7b4
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-20 20:49:22 +0300

    Z - Hard Compare in class Problem code force

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble() ;
-        double b = scanner.nextDouble() ;
 
 
     }
