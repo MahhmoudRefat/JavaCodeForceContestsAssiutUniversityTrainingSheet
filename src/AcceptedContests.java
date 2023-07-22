41d954d685966a0787b9378f3d910d9e3cd773cf
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-22 12:59:44 +0300

    C. Even, Odd, Positive and Negative in class Problem code force

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -238,4 +238,28 @@ Scanner scanner = new Scanner(System.in);
             System.out.println("NO");
         }
      */
+
+    /*
+    C. Even, Odd, Positive and Negative
+     Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // number of integers to check
+
+        int evenCount = 0, oddCount = 0, positiveCount = 0, negativeCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            }
+        }
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,5 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // number of integers to check
 
-        int evenCount = 0, oddCount = 0, positiveCount = 0, negativeCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-
-            if (num % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
-            }
-
-            if (num > 0) {
-                positiveCount++;
-            } else if (num < 0) {
-                negativeCount++;
-            }
-        }
-
-        System.out.println("Even: " + evenCount);
-        System.out.println("Odd: " + oddCount);
-        System.out.println("Positive: " + positiveCount);
-        System.out.println("Negative: " + negativeCount);
-
     }
 }
