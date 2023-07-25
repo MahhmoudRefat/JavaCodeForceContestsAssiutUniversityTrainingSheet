933ad51e80164d718f5668caba67c422a584c2ca
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-25 19:07:58 +0300

    D - Fixed Password Problem code force

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // number of integers to check
-
+        while (true) {
+            int pass = sc.nextInt();
+            if (pass == 1999) {
+                System.out.println("Correct");
+                System.exit(0);
+            } else {
+                System.out.println("Wrong");
+            }
+        }
     }
 }
