85b43e1dce8a58bc39cf9075f70165a61a45207a
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-31 09:25:03 +0300

    G - Factorial Problem code force

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        while (n>0){
+            int t = sc.nextInt();
+            long fac = 1 ;
+            for (int i =1 ; i<=t ; i++){
+                fac *= i ;
+            }
+            System.out.println(fac);
+            n--;
+        }
 
+      /*  while (n--) {
+            int fact = 1;
+            for (int i = 0; i < n; i++) {
+                fact = 1;
+                int numbers = sc.nextInt();
+                for (int j = 1; j <= numbers; j++) {
+                    fact *= j;
+                }
+                System.out.println(fact);
+            }
+        }*/
     }
 }
