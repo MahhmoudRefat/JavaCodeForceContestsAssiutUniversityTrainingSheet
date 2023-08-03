ea226f740e64f158be0cd858c32d27d65834a667
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-03 18:42:20 +0300

    I - Palindrome Problem in class   code force

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -324,4 +324,20 @@ G - Factorial ***********
        else System.out.println("NO");
     }
      */
+    /*
+    I - Palindrome
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = "" ;
+        for (int i = a.length()-1;i>=0;i--){
+            b += a.charAt(i) ;
+        }
+        int a1 , b2 ;
+        a1 = parseInt(a) ;
+        b2 = parseInt(b);
+        System.out.println(b2);
+        if (a1 == b2 ) System.out.println("YES");
+        else System.out.println("NO");
+    }
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        String b = "" ;
-        for (int i = a.length()-1;i>=0;i--){
-            b += a.charAt(i) ;
-        }
-        int a1 , b2 ;
-        a1 = parseInt(a) ;
-        b2 = parseInt(b);
-        System.out.println(b2);
-        if (a1 == b2 ) System.out.println("YES");
-        else System.out.println("NO");
     }
 }
