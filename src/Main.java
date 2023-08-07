c9b4df3339e9aaf305b0bf318aaf459d1464959b
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-07 10:25:41 +0300

    J. Primes from 1 to n in class   code force

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Main {
+    static Boolean ISPrime(int i){
+        if (i == 2 ) return true;
+        for (int k = 2 ;k <= i/2 ; k++)
+        {
+            if (i % k == 0 ){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        int n = sc.nextInt();
+        for (int i = 2 ; i <= n ; i++ ){
+            if(ISPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+
     }
+
 }
