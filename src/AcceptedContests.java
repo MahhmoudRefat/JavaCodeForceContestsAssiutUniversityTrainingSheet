205d54b053402055a349a76901c50450791a3c40
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-07 10:26:37 +0300

    J. Primes from 1 to n in class   code force

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -340,4 +340,32 @@ G - Factorial ***********
         else System.out.println("NO");
     }
      */
+
+    /*
+    J. Primes from 1 to n
+
+     static Boolean ISPrime(int i){
+        if (i == 2 ) return true;
+        for (int k = 2 ;k <= i/2 ; k++)
+        {
+            if (i % k == 0 ){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 2 ; i <= n ; i++ ){
+            if(ISPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+     */
 }
