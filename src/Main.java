11cff7896776dc27f1a0074dd4cc6699aaa2ecdf
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-09-21 09:56:10 +0300

    M - Lucky Numbers code force

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i=1 ; i <= n ;i++){
-            if (n % i ==0 ){
-                System.out.println(i);
+        int m = sc.nextInt();
+        boolean v = false;
+        for (int i = n; i <= m; i++) {
+            int a = i;
+            boolean flag = false;
+            while (a > 0) {
+                int  x = a % 10;
+                if (x != 7 && x != 4) {
+                    flag = true;
+                }
+                a /= 10;
             }
+            if (!flag) {
+                System.out.print(i + " ");
+                v = true;
+            }
+
         }
+        if (!v) System.out.println("-1");
 
     }
 
