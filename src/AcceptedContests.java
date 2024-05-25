27294caca8c76c3a4b48df7b6a4173f9d0cbd89c
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-05-25 18:46:33 +0300

    new organize for the file third group

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -322,18 +322,7 @@ G - Factorial ***********
             n--;
         }
      */
-    /*
-    H - One Prime
-           Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int c = 0 ;
-       for (int i = 1 ; i <= n /2 ;i++){
-           if (n % i == 0 ) c++ ;
-       }
-       if (c == 1 ) System.out.println("YES");
-       else System.out.println("NO");
-    }
-     */
+
 
 
     /*
---
+++ src/H_One_Prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class H_One_Prime {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0 ;
+        for (int i = 1 ; i <= n /2 ;i++){
+            if (n % i == 0 ) c++ ;
+        }
+        if (c == 1 ) System.out.println("YES");
+        else System.out.println("NO");
+    }
+
+
+}
+
+
+
