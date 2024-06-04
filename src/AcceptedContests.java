b57675e2e2a78d62eca226d534b539793ef69b88
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-06-04 08:50:38 +0300

    new organize for the file ten group

--- src/AcceptedContests.java
+++ src/AcceptedContests.java
@@ -15,29 +15,8 @@ public class AcceptedContests {
         System.out.println("Hello, "+s);
         */
 
-/*
-B - Basic Data Types
-Scanner scanner = new Scanner(System.in);
-        int intvariable = scanner.nextInt();
-        long longvariable = scanner.nextLong();
-        char charvariable = scanner.next().charAt(0);
-        float floatvariable = scanner.nextFloat();
-        double doubvariable = scanner.nextDouble()  ;
-        System.out.println(intvariable);
-        System.out.println(longvariable);
-        System.out.println(charvariable);
-        System.out.println(floatvariable);
-        System.out.println(doubvariable);
- */
-    /*
-C - Simple Calculator
-     Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        long y = scanner.nextLong();
-        System.out.println(x + " + " + y +" = "+ (x+y));
-        System.out.println(x + " * " + y +" = "+ (x*y));
-        System.out.println(x + " - " + y +" = "+ (x-y));
-     */
+
+
 
 
 
---
+++ src/B_BasicData_Types.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class B_BasicData_Types {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int intvariable = scanner.nextInt();
+        long longvariable = scanner.nextLong();
+        char charvariable = scanner.next().charAt(0);
+        float floatvariable = scanner.nextFloat();
+        double doubvariable = scanner.nextDouble()  ;
+        System.out.println(intvariable);
+        System.out.println(longvariable);
+        System.out.println(charvariable);
+        System.out.println(floatvariable);
+        System.out.println(doubvariable);
+    }
+}
