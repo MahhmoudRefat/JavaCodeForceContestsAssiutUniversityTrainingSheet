e595bf696b288c194f84502410b7a78e7af35989
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-07-24 10:25:10 +0300

    start the Asuit sheet again with problem B second one

--- src/C_Simple_Calculator.java
+++ src/C_Simple_Calculator.java
@@ -4,9 +4,9 @@ public class C_Simple_Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        long y = scanner.nextLong();
+        int y = scanner.nextInt();
         System.out.println(x + " + " + y +" = "+ (x+y));
-        System.out.println(x + " * " + y +" = "+ (x*y));
+        System.out.println(x + " * " + y +" = "+ ((long)x*y));
         System.out.println(x + " - " + y +" = "+ (x-y));
     }
 }
